8bc6b6e7822c6207d390d87bf0bf811b8eff730c
Barla Fam <nd4zmoe@gmail.com>
2026-04-23 10:49:42 +0700

    Update service worker version to 1.5

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
  * SERVICE WORKER uang famBARLA (ENTERPRISE SECURITY & CACHE LIMIT)
- * Versi 1.4
+ * Versi 1.5
  */
 
-const APP_VERSION = '1.4'; 
+const APP_VERSION = '1.5'; 
 const CACHE_PREFIX = 'uang-fambarla-';
 const CACHE_STATIC = CACHE_PREFIX + 'static-v' + APP_VERSION;
 const CACHE_DYNAMIC = CACHE_PREFIX + 'dynamic-v' + APP_VERSION;
